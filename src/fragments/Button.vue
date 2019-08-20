5cae6be67c6e8b33e20715d4bd90d827a69cf0f1
Ashwin Vishwakarma <ashwin.shekher>
2019-08-21 01:56:31 +0530

    #8, #6, #3

---
+++ src/fragments/Button.vue
@@ -0,0 +1,35 @@
+<template>
+ 
+</template>
+
+<script>
+export default {
+  name: "Button",
+  components: {},
+  data: () => {
+    return {
+      displayText: "DOWNLOAD"
+    };
+  }
+};
+</script>
+
+
+<style>
+.button {
+  width: 192px;
+  height: 53px;
+  left: 895px;
+  top: 669px;
+  background: linear-gradient(180deg, #03d0b8 0%, #005c71 100%);
+  border-radius: 8px;
+  font-size: 18px;
+  line-height: 25px;
+  /* identical to box height */
+
+  color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center; 
+}
+</style>
